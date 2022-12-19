06cfe0588a23ceb110569b098df93e5b969aa9d2
Provismet <rhys.williams343@gmail.com>
2022-12-19 18:03:58 +0000

    Add mimicry cheat sheet to voidlily notes.

--- src/main/java/com/provismet/personalOrigins/originClasses/voidlily/VoidLily.java
+++ src/main/java/com/provismet/personalOrigins/originClasses/voidlily/VoidLily.java
@@ -12,6 +12,26 @@ public class VoidLily {
     
     public static final StatusEffect VOIDPOISON_EFFECT = new VoidPoison();
 
+    /*
+     * Mimicry Value Cheat Sheet
+     *  0 : Normal (Poison on Hit)
+     *  1 : Fire Resist
+     *  2 : Blindness on Hit
+     *  3 : Saturation
+     *  4 : Jump Boost
+     *  5 : Regeneration
+     *  6 : Night Vision
+     *  7 : Weakness on Hit
+     *  8 : Wither on Hit
+     *  9 : Water Breathing
+     * 10 : Thorns
+     * 11 : Glow
+     * 12 : Speed
+     * 13 : Void Corruption on Hit
+     * 14 : Armour Bonus
+     * ## : Line-of-Sight Teleport
+     */
+
     public static Identifier identifier (String path) {
         return new Identifier("voidlily", path);
     }
