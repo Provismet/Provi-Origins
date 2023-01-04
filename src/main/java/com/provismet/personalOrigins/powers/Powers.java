5a2988caed8f22d0bac633562f551ccdeb62b504
Provismet <rhys.williams343@gmail.com>
2023-01-04 05:48:05 +0000

    Add additional keywords.

--- src/main/java/com/provismet/personalOrigins/powers/Powers.java
+++ src/main/java/com/provismet/personalOrigins/powers/Powers.java
@@ -9,8 +9,14 @@ public class Powers {
 
     // Generic field names
     public static final String ENTITY_ACTION = "entity_action";
+    public static final String ENTITY_CONDITION = "entity_condition";
     public static final String BIENTITY_ACTION = "bientity_action";
     public static final String BIENTITY_CONDITION = "bientity_condition";
+    public static final String ITEM_ACTION = "item_action";
+    public static final String ITEM_CONDITION = "item_condition";
+    public static final String KEY = "key";
+    public static final String COOLDOWN = "cooldown";
+    public static final String HUD_RENDER = "hud_render";
 
     // There are no "simple" powers in this mod, they would go here otherwise.
 
