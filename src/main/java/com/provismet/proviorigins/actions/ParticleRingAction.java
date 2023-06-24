5e5b598a8c90be0a272783455f00cbc51008fc29
Provismet <17149901+Provismet@users.noreply.github.com>
2023-06-24 15:55:33 +0100

    Add *trigonometry* to particle ring.
    The particle ring action now has the added ability to create a ring relative to the direction that the player is facing. Two powers have been updated to use this for aesthetics.

--- src/main/java/com/provismet/proviorigins/actions/ParticleRingAction.java
+++ src/main/java/com/provismet/proviorigins/actions/ParticleRingAction.java
@@ -1,21 +1,30 @@
 package com.provismet.proviorigins.actions;
 
+import org.joml.Vector3f;
+
 import com.provismet.proviorigins.powers.Powers;
 
+import io.github.apace100.apoli.data.ApoliDataTypes;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
+import io.github.apace100.apoli.util.Space;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 public class ParticleRingAction {
     private static final String PARTICLE_LABEL = "particle";
     private static final String STEP_LABEL = "step_size";
     private static final String COUNT_LABEL = "count";
+    private static final String DX_LABEL = "offset_x";
     private static final String DY_LABEL = "offset_y";
+    private static final String DZ_LABEL = "offset_z";
+    private static final String SPACE_LABEL = "space";
     private static final String SPEED_LABEL = "speed";
+    private static final String GROUND_LABEL = "on_ground";
 
     public static void action (SerializableData.Instance data, Entity entity) {
         if (entity.world.isClient) return;
@@ -23,16 +32,31 @@ public class ParticleRingAction {
         final float radius = data.getFloat(Powers.RADIUS);
         final float step = data.getFloat(STEP_LABEL) / MathHelper.DEGREES_PER_RADIAN;
         final int count = data.getInt(COUNT_LABEL);
-        final double dy = data.getDouble(DY_LABEL);
+        final Space space = data.get(SPACE_LABEL);
         final double speed = data.getDouble(SPEED_LABEL);
+        final boolean onGround = data.getBoolean(GROUND_LABEL);
         final ParticleEffect particles = data.get(PARTICLE_LABEL);
+
+        Vector3f offsets = new Vector3f(data.getFloat(DX_LABEL), data.getFloat(DY_LABEL), data.getFloat(DZ_LABEL));
+        space.toGlobal(offsets, entity);
+        Vec3d initial = entity.getPos();
+        initial = initial.add(offsets.x, offsets.y, offsets.z);
+
+        final float pitchRadians = (MathHelper.PI / 2) - (entity.getPitch() / MathHelper.DEGREES_PER_RADIAN);
+        final float yawRadians = -entity.getHeadYaw() / MathHelper.DEGREES_PER_RADIAN;
 
         ServerWorld sWorld = (ServerWorld)entity.world;
         for (float angle = 0; angle < 2 * MathHelper.PI; angle += step) {
-            double x = entity.getX() + (radius * MathHelper.sin(angle));
-            double y = entity.getY() + dy;
-            double z = entity.getZ() + (radius * MathHelper.cos(angle));
-            sWorld.spawnParticles(particles, x, y, z, count, 0.0, 0.0, 0.0, speed);
+            Vec3d point = initial.add(radius * MathHelper.sin(angle), 0.0, radius * MathHelper.cos(angle));
+
+            if (!onGround) {
+                point = point.subtract(initial);
+                point = point.rotateX(pitchRadians);
+                point = point.rotateY(yawRadians);
+                point = point.add(initial);
+            }
+
+            sWorld.spawnParticles(particles, point.x, point.y, point.z, count, 0.0, 0.0, 0.0, speed);
         }
     }
 
@@ -42,8 +66,12 @@ public class ParticleRingAction {
                 .add(Powers.RADIUS, SerializableDataTypes.FLOAT)
                 .add(STEP_LABEL, SerializableDataTypes.FLOAT)
                 .add(COUNT_LABEL, SerializableDataTypes.INT, 1)
-                .add(DY_LABEL, SerializableDataTypes.DOUBLE, 0.25)
+                .add(DX_LABEL, SerializableDataTypes.FLOAT, 0.0f)
+                .add(DY_LABEL, SerializableDataTypes.FLOAT, 0.25f)
+                .add(DZ_LABEL, SerializableDataTypes.FLOAT, 0.0f)
+                .add(SPACE_LABEL, ApoliDataTypes.SPACE, Space.LOCAL)
                 .add(SPEED_LABEL, SerializableDataTypes.DOUBLE, 0.0)
+                .add(GROUND_LABEL, SerializableDataTypes.BOOLEAN, true)
                 .add(PARTICLE_LABEL, SerializableDataTypes.PARTICLE_EFFECT_OR_TYPE),
             ParticleRingAction::action);
     }
