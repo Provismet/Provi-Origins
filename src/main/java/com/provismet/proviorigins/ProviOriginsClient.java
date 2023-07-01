4f3c547726862bee6ec3673365b483662ca63cbb
Provismet <17149901+Provismet@users.noreply.github.com>
2023-07-01 03:28:03 +0100

    Resolve Sodium incompatibility.
    The previous method that applied the Sleep fog was incompatible with Sodium (which implements its own method for applying StatusEffectFogModifier instances. The new method is compatible with Sodium and vanilla.

--- src/main/java/com/provismet/proviorigins/ProviOriginsClient.java
+++ src/main/java/com/provismet/proviorigins/ProviOriginsClient.java
@@ -4,10 +4,12 @@ import org.lwjgl.glfw.GLFW;
 
 import com.provismet.proviorigins.extras.ModelLayerRegistry;
 import com.provismet.proviorigins.extras.RendererRegistry;
+import com.provismet.proviorigins.extras.SleepFogModifier;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.render.BackgroundRenderer;
 
 public class ProviOriginsClient implements ClientModInitializer {
     public static KeyBinding tertiaryActive = KeyBindingHelper.registerKeyBinding(new KeyBinding(
@@ -26,5 +28,7 @@ public class ProviOriginsClient implements ClientModInitializer {
     public void onInitializeClient () {
         ModelLayerRegistry.register();
         RendererRegistry.register();
+
+        BackgroundRenderer.FOG_MODIFIERS.add(0, new SleepFogModifier());
     }
 }
---
+++ src/main/java/com/provismet/proviorigins/extras/SleepFogModifier.java
@@ -0,0 +1,29 @@
+package com.provismet.proviorigins.extras;
+
+import com.provismet.proviorigins.content.StatusEffects.StatusEffects;
+
+import net.minecraft.client.render.BackgroundRenderer.FogData;
+import net.minecraft.client.render.BackgroundRenderer.FogType;
+import net.minecraft.client.render.BackgroundRenderer.StatusEffectFogModifier;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+
+public class SleepFogModifier implements StatusEffectFogModifier {
+    @Override
+    public StatusEffect getStatusEffect () {
+        return StatusEffects.SLEEP;
+    }
+
+    @Override
+    public void applyStartEndModifier (FogData fogData, LivingEntity entity, StatusEffectInstance effect, float viewDistance, float tickDelta) {
+        if (fogData.fogType == FogType.FOG_SKY) {
+            fogData.fogStart = 0f;
+            fogData.fogEnd = 2f;
+        } else {
+            fogData.fogStart = 0.5f;
+            fogData.fogEnd = 5f;
+        }
+    }
+    
+}
