0e790c3b7ce9d3d20241e28e102629260b49f11b
Provismet <17149901+Provismet@users.noreply.github.com>
2023-09-06 01:42:01 +0100

    Gave y offset options to particle beam.

--- src/main/java/com/provismet/proviorigins/actions/bientity/ParticleBeamAction.java
+++ src/main/java/com/provismet/proviorigins/actions/bientity/ParticleBeamAction.java
@@ -14,6 +14,8 @@ import net.minecraft.util.math.Vec3d;
 public class ParticleBeamAction {
     private static final String STEP_LABEL = "step";
     private static final String SPEED_LABEL = "speed";
+    private static final String ACTOR_OFFSET_LABEL = "actor_offset_y";
+    private static final String TARGET_OFFSET_LABEL = "target_offset_y";
 
     public static void action (SerializableData.Instance data, Pair<Entity, Entity> bientity) {
         if (bientity.getLeft().getWorld().isClient()) return;
@@ -28,10 +30,17 @@ public class ParticleBeamAction {
         final double speed = data.getDouble(SPEED_LABEL);
         final int count = data.getInt(Powers.COUNT);
 
+        final double actor_y = data.getDouble(ACTOR_OFFSET_LABEL);
+        final double target_y = data.getDouble(TARGET_OFFSET_LABEL);
+
         Vec3d currentPos = actor.getPos();
         final double maxDistance = actor.getPos().distanceTo(target.getPos());
 
-        Vec3d eachStep = (new Vec3d(target.getX() - actor.getX(), target.getEyeY() - actor.getEyeY(), target.getZ() - actor.getZ())).normalize().multiply(step);
+        Vec3d eachStep = (new Vec3d(
+            target.getX() - actor.getX(),
+            (target.getEyeY() + actor_y) - (actor.getEyeY() + target_y),
+            target.getZ() - actor.getZ())
+        ).normalize().multiply(step);
         
         while (currentPos.distanceTo(actor.getPos()) < maxDistance) {
             sWorld.spawnParticles(particle, currentPos.x, currentPos.y, currentPos.z, count, 0.0, 0.0, 0.0, speed);
@@ -45,7 +54,9 @@ public class ParticleBeamAction {
                 .add(STEP_LABEL, SerializableDataTypes.DOUBLE)
                 .add(Powers.PARTICLE, SerializableDataTypes.PARTICLE_EFFECT_OR_TYPE)
                 .add(SPEED_LABEL, SerializableDataTypes.DOUBLE, 0.0)
-                .add(Powers.COUNT, SerializableDataTypes.INT, 1),
+                .add(Powers.COUNT, SerializableDataTypes.INT, 1)
+                .add(ACTOR_OFFSET_LABEL, SerializableDataTypes.DOUBLE, 0.0)
+                .add(TARGET_OFFSET_LABEL, SerializableDataTypes.DOUBLE, 0.0),
             ParticleBeamAction::action);
     }
 }
