d4c2497f88841e5a6439cf52fd3a87b88a9d8855
Provismet <17149901+Provismet@users.noreply.github.com>
2025-01-05 22:13:00 +0000

    Add constants for resource sprite locations.

---
+++ src/main/java/com/provismet/proviorigins/utility/constants/SpriteLocations.java
@@ -0,0 +1,12 @@
+package com.provismet.proviorigins.utility.constants;
+
+import com.provismet.proviorigins.ProviOriginsMain;
+import net.minecraft.util.Identifier;
+
+public interface SpriteLocations {
+    Identifier ALRAUNE_LIFE_SAP_RESOURCE = ProviOriginsMain.identifier("textures/gui/resources/alraune_life_sap_resource.png");
+    Identifier EXTRA_RESOURCES = ProviOriginsMain.identifier("textures/gui/resources/extra_resources.png");
+    Identifier SPLINTER_CLONE_RESOURCE = ProviOriginsMain.identifier("textures/gui/resources/splinter_clone_resource.png");
+    Identifier VOIDLILY_RESOURCES = ProviOriginsMain.identifier("textures/gui/resources/voidlily_resources.png");
+    Identifier VOIDLILY_EXTRA_RESOURCES = ProviOriginsMain.identifier("textures/gui/resources/voidlily_extra_resources.png");
+}
